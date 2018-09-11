055b677c1dd4ab6cf25578a910c43febbe854137
irohitb <irohitbhatia@gmail.com>
2018-09-12 02:14:43 +0530

    Testing charts

--- index.js
+++ index.js
@@ -15,3 +15,15 @@ AppRegistry.registerComponent(appName, () => App);
 
 //React Native Performance 
 //https://stackoverflow.com/questions/52020855/react-native-performance-issue/52230865?noredirect=1#comment91480937_52230865
+
+
+//React Native Motion 
+//https://medium.com/react-native-motion/transition-challenge-9bc9fdef56c7
+
+//eact Native Navigating between screens
+//https://facebook.github.io/react-native/docs/navigation
+
+//npm install --save react-navigation
+
+//Relevant Git Repository 
+//https://github.com/msenejoa/crypto-markets -> For graphs
--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
-import { Platform } from 'react-native';
-import { createStore, applyMiddleware, compose } from 'redux';
 
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer.js';
 
