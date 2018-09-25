8c351ba8172cb7e1da23da3533a9057a2d446737
irohitb <irohitbhatia@gmail.com>
2018-09-26 04:38:48 +0530

    Intitsl Navbar design Done

--- src/components/BottomNavigation.js
+++ src/components/BottomNavigation.js
@@ -1,13 +1,24 @@
 import React, {PureComponent} from 'react';
 import { 
-    StyleSheet, 
-    Text, 
-    View
-} from 'react-native';
+    StyleSheet,  
+    View, 
+    Text
+  } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Icons from 'react-native-vector-icons/Entypo';
+import Icons1 from "react-native-vector-icons/EvilIcons";
+import Icons2 from "react-native-vector-icons/Feather";
 
-
+//user
 class BottomNavigation extends PureComponent {
+
+    constructor() {
+        super() 
+        this.home = (<Icon name="bitcoin" size={35} color="#005cb2" />);
+        this.News = (<Icons name="news" size={35} color="#005cb2" />);
+        this.Exchange = (<Icons2 name="bar-chart-2" size={35} color="#005cb2" />);
+        this.about = (<Icons2 name="user" size={35} color="#005cb2" />)
+    }
 
     state = {
         home: true, 
@@ -19,9 +30,13 @@ class BottomNavigation extends PureComponent {
 
     render () {
         return (
-            <View styles={headerContainer}> 
-                <Text style={header}> This is Bottom Navigation</Text>
-            </View>
+     
+                <View style={styles.NavBarBottom}>
+                    <Text>{this.home}</Text>
+                    <Text> {this.News}</Text>
+                    <Text> {this.Exchange}</Text>
+                    <Text> {this.about}</Text>
+                 </View>
         )
     }
 }
@@ -29,23 +44,12 @@ class BottomNavigation extends PureComponent {
 export default BottomNavigation;
 
 const styles = StyleSheet.create({
-headerContainer : {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: 'center',
-    backgroundColor: "white",
-    borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0
-  },
-  header: {
-    marginTop: 20,
-    padding: 10,
-    fontSize: 16,
-    color: "black",
-  },
-})
-
-const {
-    headerContainer,
-    header
-} = styles
+    NavBarBottom : {
+      borderTopWidth: 2,
+      borderTopColor: '#fbc02d',
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-around",
+      padding: 5
+    }
+  })
