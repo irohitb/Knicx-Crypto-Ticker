78fddcbf208fa4c6db13aac6c5018250bd75f67e
irohitb <irohitbhatia@gmail.com>
2018-10-02 01:30:41 +0530

    git stash git checkout master error

--- src/urls.js
+++ src/urls.js
@@ -1,8 +1,14 @@
+//CoinCap
 export const ApiCoinCap = "https://coincap.io/front"
 export const CurrencyRateLink = "https://ratesapi.io/api/latest?base=USD"
 export const coinCapHistory = "https://coincap.io/history/" //incomplete URL 
 export const coinCapComplete = "https://coincap.io/page/" //incompete URL 
+
+//Social Media
 export const RedditFetch = "https://www.reddit.com/search.json?q=" //Incomplete URL
+
+//News
+export const cryptoCompareNews = "https://min-api.cryptocompare.com/data/v2/news/?lang=EN"
 
 
 //Exchanges 
